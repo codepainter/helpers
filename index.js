738f88e00cf84a612325dee54977112f87c7fc5c
Abiyasa <abiyasa2410@gmail.com>
2020-04-25 19:23:14 +0700

    Feature: heapdiff and byte convertion

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 module.exports = {
-  getEnv
+  getEnv,
+  convert,
+  heapdiff
 }
 
 let cache = {}
-
 function getEnv (key, value) {
   if (!(key in process.env)) {
     if (value) return value
@@ -15,4 +16,22 @@ function getEnv (key, value) {
   cache[key] = process.env[key]
 
   return process.env[key]
+}
+
+function convert (byte) {
+  const arr = ['Byte', 'KB', 'MB', 'GB', 'TB']
+  let idx = 0
+  while (byte > 999 && idx < 5) {
+    byte = Math.round((byte * 100) / 1000) / 100
+    idx++
+  }
+  let size
+  if (idx >= 0 && idx <= arr.length - 1) size = arr[idx]
+  if (byte < 0) byte = 0
+  return `${byte} ${size}`
+}
+
+const bytes = require('bytes')
+function heapdiff (byte) {
+  return bytes(Math.abs(process.memoryUsage().heapUsed - byte))
 }
